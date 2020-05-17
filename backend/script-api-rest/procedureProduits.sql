a43282ce4f158e87580cb795ba8afa3762b8565f
nightcoding-ai <64195967+nightcoding-ai@users.noreply.github.com>
2020-05-17 12:21:55 +0200

    Delete procedureProduits.sql

--- backend/script-api-rest/procedureProduits.sql
+++
@@ -1,6 +0,0 @@
-----------ACCES A TOUTE LA LISTE DES PRODUITS DU SITE-------------------------
-CREATE PROCEDURE getProduits()
-BEGIN
- SELECT LIST('<p>' || nomProd || '</p>',  '')
- FROM produits 
-END;
