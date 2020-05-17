6a31267d4548652d36a1ba20c4777e3aa49e542a
nightcoding-ai <64195967+nightcoding-ai@users.noreply.github.com>
2020-05-17 12:21:24 +0200

    Delete procedureCat.sql

--- backend/script-api-rest/procedureCat.sql
+++
@@ -1,7 +0,0 @@
-----------ACCES AUX CATEGORIES DE PRODUITS PROPOSEES--------------------
-
-CREATE PROCEDURE "DBA"."getCategorie"()
-BEGIN
- SELECT LIST('<p>' || nomCat || '</p>',  '')
- FROM categories
-END
